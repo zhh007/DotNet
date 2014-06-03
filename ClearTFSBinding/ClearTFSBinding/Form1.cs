89fa393963ac95d9dfa1b20082d6f901190189d7
zhh007 <zhh9113@hotmail.com>
2014-06-03 12:39:05 +0800

    test

--- ClearTFSBinding/ClearTFSBinding/Form1.cs
+++ ClearTFSBinding/ClearTFSBinding/Form1.cs
@@ -15,7 +15,7 @@ namespace ClearTFSBinding
     {
         public Form1()
         {
-            InitializeComponent(); //dddd
+            InitializeComponent(); //zhh007
         }
 
         private void button1_Click(object sender, EventArgs e)
