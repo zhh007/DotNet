a8dca6df5c1a58b4a06e19dab528b7bfc9b89bc7
zhh007 <zhh9113@hotmail.com>
2015-06-17 20:05:33 +0800

    unity demo

---
+++ ASPNETMVCIOC/MVC4_Unity/Demo.Service/ServiceA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Demo.Interface;
+
+namespace Demo.Service
+{
+    public class ServiceA : IServiceA
+    {
+        #region IServiceA Members
+
+        public string Say()
+        {
+            return "I am Service A.";
+        }
+
+        #endregion
+    }
+}
---
+++ ASPNETMVCIOC/MVC4_Unity/Demo.Service/ServiceB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Demo.Interface;
+
+namespace Demo.Service
+{
+    public class ServiceB : IServiceB
+    {
+        #region IServiceB Members
+
+        public string Write()
+        {
+            return "I am Service B.";
+        }
+
+        #endregion
+    }
+}
