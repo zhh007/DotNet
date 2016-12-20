27cd45f5c719bb56f447d3f90abdc510cc900523
詹華 <zhh9113@hotmail.com>
2016-12-20 14:22:26 +0800

    update

--- MVC5Demo/MVC5Demo/Controllers/HomeController.cs
+++ MVC5Demo/MVC5Demo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Demo.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC5Demo.Controllers
 {
+    //[AuthorizationPrivilegeFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ MVC5Demo/MVC5Demo/Filters/AuthorizationPrivilegeFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC5Demo.Filters
+{
+    public class AuthorizationPrivilegeFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary{
+                        { "controller", "Account" },{ "action", "Login" }});
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
