921954a588791a775949714c3af4f8583c4ba310
詹華 <zhh9113@hotmail.com>
2016-12-20 17:19:14 +0800

    update

---
+++ MVC5Demo/MVC5Demo/Controllers/WeatherController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> ShowAsync()
+        {
+            var str = await GetWeather();
+
+            ViewBag.Weather = str;
+
+            return View();
+        }
+
+        public async Task<string> GetWeather()
+        {
+            var uri = "http://api.jirengu.com/weather.php";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(uri);
+                return (await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        public async Task<string> GetGithubReadme()
+        {
+            var uri = "http://api.jirengu.com/readme2html.php?url=https://github.com/aspnet/Mvc";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(uri);
+                return (await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        public async Task<ActionResult> ShowLongTaskAsync()
+        {
+            var task1 = GetWeather();
+            var task2 = GetGithubReadme();
+
+            await Task.WhenAll(task1, task2);
+
+            ViewBag.Weather = task1.Result;
+            ViewBag.Readme = task2.Result;
+
+            return View();
+        }
+    }
+}
